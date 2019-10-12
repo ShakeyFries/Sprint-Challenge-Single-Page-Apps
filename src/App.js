0d16c1067c7261482b2b9c06934a155aff4d5421
Chris Daley <Daleys@Chriss-iMac.local>
2019-10-11 20:38:26 -0600

    finished the Router and Link portion

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage";
+import CharacterCard from "./components/CharacterCard";
+
 
 export default function App() {
   return (
     <main>
       <Header />
       <div>
-        <WelcomePage />
-        <Route exact path="/" component={CharacterList} />
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/character-list" component={CharacterList} />
+        <Route path="/header" component={Header} />
+        <Route path="/character-card" component={CharacterCard} />
       </div>
     </main>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 
 function CharacterCard (props) {
   console.log(props);
-  const { name, species, gender, status, id } = props.character;
+  const { name, species, gender, status, id, image } = props.character;
   return (
 
     <div className ="character-card">
-      <Link to={`/results/${id}`}> <h2>{name}</h2 ></Link>
+      <Link to={`info/results/${id}`}> <h2>{name}</h2 ></Link>
+      <div className="character-image">
+        <img src={image} alt={`photo of ${name}`}/>
+      </div>
       <div className ="character-species">
         Species: <em>{species}</em>
       </div>
