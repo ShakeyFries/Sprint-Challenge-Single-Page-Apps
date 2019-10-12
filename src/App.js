335c75af11a8bcfbcf05a22cee04923fa1bcb8d1
Chris Daley <Daleys@Chriss-iMac.local>
2019-10-11 20:09:21 -0600

    got my info to appear, saving progress here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <div>
+        <WelcomePage />
+        <Route exact path="/" component={CharacterList} />
+      </div>
     </main>
   );
 }
+
